0efc4b2b1ff482f551da9d474999b524f9902673
CookieD93 <dani9035@edu.easj.dk>
2017-11-09 11:36:53 +0100

    Added new index that redirects to the Index Controller + Removed old content from nav

--- Controllers/Index.php
+++ Controllers/Index.php
@@ -13,9 +13,9 @@ if(isset($_GET['fejl'])){
 }
 else{$Fejlmeddelelse = "";}
 
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 Twig_Autoloader::register();
-$loader = new Twig_Loader_Filesystem('views');
+$loader = new Twig_Loader_Filesystem('../views');
 $twig = new Twig_Environment($loader, array(
 'auto_reload' => true
 ));
--- Views/nav.php
+++ Views/nav.php
@@ -1,5 +1,5 @@
 <nav>
-    <a href="../Index.php">Forside</a>
+    <a href="../Controllers/Index.php">Forside</a>
     <a href="../Controllers/PhpTest.php">PhP Side</a>
     <a href="../Controllers/Calculator.php">Calculator</a>
 </nav>
