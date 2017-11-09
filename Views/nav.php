f6128e7af3f0588f73882479636111b4c080bad7
CookieD93 <dani9035@edu.easj.dk>
2017-11-09 11:20:51 +0100

    index in root

--- Views/nav.php
+++ Views/nav.php
@@ -1,12 +1,5 @@
-{#<nav>
-    <a <?php echo ($page == "pp")?"class='active'":""; ?> href="../Controllers/Index.php">Forside</a>
-
-    <a <?php echo ($page == "qq")?"class='active'":""; ?>  href="../Controllers/PhpTest.php">PhP Side</a>
-    <a <?php echo ($page == "calc")?"class='active'":""; ?>  href="../Controllers/Calculator.php">Calculator</a>
-</nav>#}
-
 <nav>
-    <a href="../Controllers/Index.php">Forside</a>
+    <a href="../Index.php">Forside</a>
     <a href="../Controllers/PhpTest.php">PhP Side</a>
     <a href="../Controllers/Calculator.php">Calculator</a>
 </nav>
