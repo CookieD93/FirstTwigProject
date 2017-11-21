16411d7a3cccdeedeaed6a688b9249a968e99dcc
CookieD93 <dani9035@edu.easj.dk>
2017-11-21 11:47:45 +0100

    Rest Customer Update complete

--- Controllers/RestCustomers.php
+++ Controllers/RestCustomers.php
@@ -1,16 +1,57 @@
 <?php
-$BaseUri = "http://restcustomerservicecookie.azurewebsites.net/service1.svc/";
+$BaseUri = "http://restcustomerservicecookie.azurewebsites.net/service1.svc/customerdb/";
 $Customers="";
 
+function CallAPI($method, $url, $data = false)
+{
+    $curl = curl_init();
+    switch ($method)
+    {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+            curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data))
+            );
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data))
+    );
+
+    $result = curl_exec($curl);
+
+    curl_close($curl);
+
+    return $result;
+}
+
 if (isset($_POST['GetCustomersKnap'])&!empty($_POST['GetCustomersFelt'])){
-    $uri = $BaseUri . "customerdb/" . $_POST['GetCustomersFelt'];
+    $uri = $BaseUri . $_POST['GetCustomersFelt'];
     $jsondata = file_get_contents($uri);
     $convertToAssociativeArray = true;
     $Customers = json_decode($jsondata, $convertToAssociativeArray);
     $Customers = array($Customers);
 }
 elseif (isset($_POST['GetCustomersKnap'])){
-    $uri = $BaseUri . "customersdb/";
+    $uri = "http://restcustomerservicecookie.azurewebsites.net/service1.svc/customersdb/";
     //if (!empty($_POST['GetCustomersFelt'])){
       //  $uri = $BaseUri . "customerdb/" . $_POST['GetCustomersFelt'];
         //print_r($uri);
@@ -23,6 +64,20 @@ elseif (isset($_POST['GetCustomersKnap'])){
    // $Customers = array($Customers);
 }
 
+if (isset($_POST['CreatueCustomerSubmit'])){
+    $Data = array("FirstName"=>$_POST['CreateCustomerFName'],"LastName"=>$_POST['CreateCustomerLName'],"Year"=>$_POST['CreateCustomerYear']);
+    $Data = json_encode($Data);
+    $result = CallAPI("POST",$BaseUri,$Data);
+}
+if (isset($_POST['UpdateCustomerSubmit'])){
+
+    $Data = array("Id"=>$_POST['UpdateCustomerId'],"FirstName"=>$_POST['CreateCustomerFName'],"LastName"=>$_POST['CreateCustomerLName'],"Year"=>$_POST['CreateCustomerYear']);
+    $Data = json_encode($Data);
+   print_r($Data);
+
+    $result = CallAPI("PUT",$BaseUri,$Data);
+
+}
 
 
 require_once '../vendor/autoload.php';
